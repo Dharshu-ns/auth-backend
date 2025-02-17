b160c701e3d773c0070ea0ead7c203b466d52d42
dharshu_ns <dharshanans54@gmail.com>
2025-02-17 23:53:17 +0530

    Server.js is formatted

--- server.js
+++ server.js
@@ -1,22 +1,23 @@
-require('dotenv').config()
+require('dotenv').config();
 const express = require("express");
-const CookieParser = require("cookie-parser")
+const CookieParser = require("cookie-parser");
 const mongoose = require("mongoose");
-const cros = require("cors");
+const cors = require("cors");
 
 const app = express();
 
-app.use(express.json);
-app.use(cros({origin:"http://localhost:3000", credentials: true}))
+app.use(express.json());
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 app.use(CookieParser());
 
-console.log(process.env.MONGO_URI)
+console.log(process.env.MONGO_URI);
+console.log("port--->", process.env.PORT);
 //Database Connection
-mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: false, useUnifiedTopology:true })
-.then(()=> console.log("DB is connected"))
-.catch((err)=> console.error(err))
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("DB is connected"))
+  .catch((err) => console.error(err));
 
-const PORT = process.env.port || 5000;
-app.listen(PORT,()=>{
-console.log(`app is running on the port ${PORT}`)
-})
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`app is running on the port ${PORT}`);
+});
