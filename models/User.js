3af01d0cab6d4c6cdc5129bf5711d9b29616f27b
dharshu_ns <dharshanans54@gmail.com>
2025-02-18 01:00:55 +0530

    updated user model to include jwtSecrete

--- models/User.js
+++ models/User.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-    name: String,
-    email:{type:String, unique: true},
-    password:String,
-    googleId:String,
-    githubId:String
-})
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    googleId: String,
+    githubId: String,
+    jwtSecret: { type: String, required: true } 
+});
 
-module.exports = mongoose.model("user", UserSchema);
+module.exports = mongoose.model("User", UserSchema);
--- utils/jwtToken.js
+++
@@ -1,9 +0,0 @@
-const jwt = require("jsonwebtoken");
-
-const generateToken = (userId) => {
-  return jwt.sign({ id: userId }, process.env.JWT_SECRETE, {
-    expiresIn: "1h",
-  });
-};
-
-module.exports = generateToken;
