85e1b4484998347c9e473cda39f016414e88224e
dharshu_ns <dharshanans54@gmail.com>
2025-02-18 01:00:06 +0530

    added user routes

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require("express");
 const CookieParser = require("cookie-parser");
 const mongoose = require("mongoose");
 const cors = require("cors");
+// Import routes
+const authRoutes = require("./routes/auth");
 
 const app = express();
 
@@ -10,12 +12,13 @@ app.use(express.json());
 app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 app.use(CookieParser());
 
-console.log(process.env.MONGO_URI);
-console.log("port--->", process.env.PORT);
-//Database Connection
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+// Database Connection
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("DB is connected"))
   .catch((err) => console.error(err));
+  
+ // Use routes
+app.use("/api", authRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
