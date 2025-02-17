739f76fa980e849abfbd98e133c0d462478bc8ea
dharshu_ns <dharshanans54@gmail.com>
2025-02-17 22:57:59 +0530

    mongoDB connection string

--- server.js
+++ server.js
@@ -3,13 +3,18 @@ const express = require("express");
 const CookieParser = require("cookie-parser")
 const mongoose = require("mongoose");
 const cros = require("cors");
-const cookieParser = require('cookie-parser');
 
 const app = express();
 
 app.use(express.json);
 app.use(cros({origin:"http://localhost:3000", credentials: true}))
-app.use(cookieParser());
+app.use(CookieParser());
+
+console.log(process.env.MONGO_URI)
+//Database Connection
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: false, useUnifiedTopology:true })
+.then(()=> console.log("DB is connected"))
+.catch((err)=> console.error(err))
 
 const PORT = process.env.port || 5000;
 app.listen(PORT,()=>{
